933ccbcb5a640dd35e15d7d3a8b1d83070fbe942
Kjetil Flovild-Midtlie <kfm@docstream.no>
2016-06-13 08:36:12 +0200

    blackFG

--- tile-gen.hs
+++ tile-gen.hs
@@ -6,7 +6,7 @@ import Data.List
 
 
 data Tile = Small Color 
-          | Big Color
+          | Dbl Color
           | NewRow
             deriving (Show,Eq)
 
@@ -16,7 +16,7 @@ type Color = Int -- TODO data type ??
 -- ctor            
 newTile :: (TileWidth,Color) -> Tile
 newTile (0,c) = Small c
-newTile (_,c) = Big c
+newTile (_,c) = Dbl c
 
 -- lovley infinite streams
 newTiles :: [TileWidth] -> [Color] -> [Tile]
@@ -24,7 +24,7 @@ newTiles ws cs = map (newTile) $ zip ws cs
 
 tileCells :: Tile -> Int
 tileCells (Small _) = 1
-tileCells (Big _) = 2
+tileCells (Dbl _) = 2
 tileCells (NewRow) = 0
 
 data InputArgs = InputArgs { 
@@ -72,19 +72,25 @@ parseArgs = do
 usage :: IO ()
 usage = do
   prog <- getProgName
-  putStrLn "Tool that generates floor/tiles (max 2 sizes, 7 colors) from random."
+  putStrLn "Tool that generates floor/tiles from random."
   putStrLn "Please give us these arguments"
   putStrLn "usage:"
-  putStrLn $ "  " ++ prog ++ " TILE-COLORS SIZE-ODDS FLOOR-WIDTH FLOOR-HEIGHT"
+  putStrLn $ "  " ++ prog ++ " <C> <S> <W> <H>"
+  putStrLn "  where \n  C -> Tile colors"
+  putStrLn "  S -> Size odds ( Tile is either 1 or 2 cell, higher num give 1 cell low odds )"
+  putStrLn "  W -> Floor width ( Number of cells before new row )"
+  putStrLn "  H -> Floor height ( Number of rows )"
+
+
   -- hmm difficult to exit here
 
 -- summary :: [Tile] -> [(Tile,Int)]
 -- summary = foldl (incr) []
 --   where
---   incr acc@(t,cnt) tile = case lookup tile acc of 
---                             Just cnt -> swapIn (tile,cnt+1) acc
---                             Nothing -> acc
---   swapIn (
+--   incr acc tile = case lookup tile acc of 
+--                     Just cnt -> swapIn (tile, cnt+1) acc
+--                     Nothing -> swapIn [(tile,1)] acc
+--   swapIn (t,cnt) (x:xs) = 
 
 
 randInts :: StdGen -> Int -> [Int]
@@ -114,28 +120,23 @@ tileRow width (x:xs)
 showTile :: Tile -> String
 showTile (NewRow) = normColor ++ "\n"
 showTile (Small color) =
-  bgColor ( nicerColor color ) ++ "└ "
-showTile (Big color) =
-  bgColor ( nicerColor color ) ++ "├   "
+                color' ( nicerColor color ) ++ "s."
+showTile (Dbl color) =
+              color' ( nicerColor color ) ++ "d  ."
 
 
 -- backto normaal
 normColor :: String
-normColor = nixEsc 0
+normColor = nixEsc 0 0
 
 -- background
-bgColor :: Int -> String
-bgColor n =
+color' :: Int -> String
+color' n =
   let bg = 40
+      fg = 30
   in 
-    nixEsc $ bg + n
+    nixEsc ( bg + n ) $ fg + 0
 
--- foreground
-fgColor :: Int -> String
-fgColor n =
-  let fg = 30
-  in
-    nixEsc $ fg + n
 
 -- avoid red+black as starters
 nicerColor :: Int -> Int
@@ -149,9 +150,8 @@ nicerColor n
   | n == 6 = 5 -- magents
   | n == 7 = 1 -- red
 
-nixEsc :: Int -> String
--- need `;` and more Int's too work w gb+fg+etc
-nixEsc n = "\ESC[" ++ show n ++ "m"
+nixEsc :: Int -> Int ->String
+nixEsc n m = "\ESC[" ++ show n ++ ";" ++ show m ++ "m"
 
 
 -- intsToLines :: [Int] -> String
